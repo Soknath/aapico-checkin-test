517318c6f8538dd8b305b5f0bd1cafb518321f1c
MIL Soknath <milsoknath@MILs-MacBook-Pro.local>
2020-04-17 11:09:30 +0700

    add build

---
+++ build/precache-manifest.39f6f5f6412c235bc0f2f49a0269483f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05235ce3d2cd4b5ef63",
+    "url": "/aapico-checkin/static/js/runtime~main.e05235ce.js"
+  },
+  {
+    "revision": "6fd1cf8d8b13203735c5",
+    "url": "/aapico-checkin/static/js/main.6fd1cf8d.chunk.js"
+  },
+  {
+    "revision": "22eeb6ccfba576c7e61f",
+    "url": "/aapico-checkin/static/js/2.22eeb6cc.chunk.js"
+  },
+  {
+    "revision": "6fd1cf8d8b13203735c5",
+    "url": "/aapico-checkin/static/css/main.aac1b552.chunk.css"
+  },
+  {
+    "revision": "22eeb6ccfba576c7e61f",
+    "url": "/aapico-checkin/static/css/2.faa3af53.chunk.css"
+  },
+  {
+    "revision": "79578709e6475dd260081170e2809c29",
+    "url": "/aapico-checkin/index.html"
+  }
+];
