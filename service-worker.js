caee9c44e9062186e84ff7e6369b4c11c8e18724
Soknath <soknath.siit@gmail.com>
2020-05-06 08:02:16 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 importScripts("/aapico-checkin-test/precache-manifest.dc1809f7cf59878fc488cce5b322fc08.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 console.log("Worker is working")
-var cacheName = 'SecondVersion';
+var cacheName = 'ThirdVersion';
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
 
